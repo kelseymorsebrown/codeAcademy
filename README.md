7c6e994e3ae1bfe84bbc9f46f4646f0480b2e07c
Kelsey Morse-Brown <kelsey@jellyvision.com>
2024-01-19 20:13:11 -0600

    Add babel

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # codeAcademy
+
 For Code Academy practice and such
+
+[ES6 Modules on CodeAcademy](https://www.codecademy.com/paths/build-web-apps-with-react/tracks/bwa-modern-javascript-modules-and-browser-compatibility/modules/es-6-modules/articles/implementing-modules-using-es-6-syntax)
+
+[browserslist](https://github.com/browserslist/browserslist)
---
+++ src/secret-messages/secret-messages.js
@@ -0,0 +1,12 @@
+import domFunctions from '../modules/dom-functions.js';
+const { toggleHiddenElement, changeToFunkyColor } = domFunctions;
+
+const buttonElement = document.getElementById('secret-button');
+const pElement = document.getElementById('secret-p');
+
+buttonElement.addEventListener('click', () => {
+  toggleHiddenElement(pElement);
+  changeToFunkyColor(buttonElement);
+});
+
+// https://www.codecademy.com/paths/build-web-apps-with-react/tracks/bwa-modern-javascript-modules-and-browser-compatibility/modules/es-6-modules/articles/implementing-modules-using-es-6-syntax
