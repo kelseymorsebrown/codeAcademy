a37502e8388168fbd9a75fc93bbd51313d2d9045
Kelsey Morse-Brown <132404623+kelseymorsebrown@users.noreply.github.com>
2024-03-22 17:24:32 -0500

    add codeacademy authorization form project

---
+++ authorization-form/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Contact from './Contact'
+
+import "./App.css";
+
+function App() {
+  return <Contact />;
+}
+
+export default App
